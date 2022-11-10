8cacec2ea42fa7475c4dab887577f5b1ef690c77
shoebabedin <shoeb.abedin@intelli.global>
2022-11-10 11:26:03 +0600

    design bug fix

--- src/Components/Common/Accordion/Accordion.js
+++ src/Components/Common/Accordion/Accordion.js
@@ -42,8 +42,10 @@ const Accordion = () => {
                         <span className="expand-bar"></span>
                       </li>
                       <li
-                        className={`answer font-normal text-base leading-6 text-[rgba(57,62,80,0.4)] ${
-                          accordionOpen === item.id && "!list-item"
+                        className={`font-normal text-base leading-6 text-[rgba(57,62,80,0.4)] list-none ${
+                          accordionOpen === item.id
+                            ? "translate-y-0 ease-in-out opacity-100 transition-all duration-300 pt-4 pr-2 pb-5 md:pb-10"
+                            : "opacity-0 -translate-y-4 transition-all duration-300 h-0"
                         }`}
                       >
                         {item.ans}
